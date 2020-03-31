fdfb737dfd6662abd0184336e67f1aa0df33a63f
PKWIPRO <62870615+PKWIPRO@users.noreply.github.com>
2020-03-31 22:25:31 +0530

    Update cleanup.sh

--- script/cleanup.sh
+++ script/cleanup.sh
@@ -3,7 +3,7 @@
 # I want to make sure that the directory is clean and has nothing left over from
 # previous deployments. The servers auto scale so the directory may or may not
 # exist.
-if [ -d /var/www/html/dist ]; then
-    rm -rf /var/www/html/dist
+if [ -d /home/ec2-user/appservice ]; then
+    rm -rf /home/ec2-user/appservice
 fi
-mkdir -vp /var/www/html/dist
+mkdir -vp /home/ec2-user/appservice
